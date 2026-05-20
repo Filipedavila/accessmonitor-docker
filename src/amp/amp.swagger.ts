a3e001b5ea76bbce5ee3b4c975a3744abb5491ed
Filipe Dávila <filipedavila6@gmail.com>
2026-05-20 18:01:38 +0100

    feat: enhance AMP documentation with detailed operation summaries and response types

--- src/amp/amp.swagger.ts
+++ src/amp/amp.swagger.ts
@@ -11,30 +11,34 @@ export const AmpDocs = {
   controller: () =>
     applyDecorators(
       ApiBasicAuth(),
-      ApiTags('amp'),
-      ApiResponse({ status: 403, description: 'Forbidden' }),
+      ApiTags('AMP - Accessibility Monitor Processing'),
     ),
   evaluateUrl: () =>
     applyDecorators(
-      ApiOperation({ summary: 'Evaluate page via url' }),
+      ApiOperation({ summary: 'Evaluate page via url and get the evaluation report', 
+          description: 'Evaluates the page at the given URL and returns a report with the evaluation results. The URL is evaluated against a set of predefined rules and checks to determine if it meets certain criteria for accessibility using QualWeb Engine.'
+      }),
       ApiResponse({
         status: 200,
         description: 'Success',
-        type: Boolean,
+        type: Object,
       }),
     ),
 
   evaluateHtml: () =>
     applyDecorators(
-      ApiOperation({ summary: 'Evaluate html code' }),
+      ApiOperation({ summary: 'Evaluate HTML code and get the evaluation report', 
+        description: 'Evaluates the provided HTML code and returns a report with the evaluation results. The HTML is evaluated against a set of predefined rules and checks to determine if it meets certain criteria for accessibility using QualWeb Engine.'
+      }),
       ApiResponse({
         status: 200,
         description: 'Success',
-        type: Boolean,
+        type: Object
       }),
     ),
 };
 export interface AmpContract {
-  evaluateUrl: (req: any, url: UrlRequestDto) => Promise<boolean>;
-  evaluateHtml: (req: any) => Promise<boolean>;
+  evaluateUrl: (req: Request, url: UrlRequestDto) => Promise<object>;
+  evaluateHtml: (req: Request) => Promise<object>;
 }
+
