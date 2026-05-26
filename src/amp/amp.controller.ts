cc9ac9a8c30eaafe4e9e5acc6f28f045ee4c4048
Filipe Dávila <filipedavila6@gmail.com>
2026-05-26 15:12:19 +0100

    feat: implement URL validation using validateUrlRestriction and remove deprecated methods and tests added

--- src/amp/amp.controller.ts
+++ src/amp/amp.controller.ts
@@ -52,7 +52,7 @@ export class AmpController implements AmpContract {
   async evaluateHtml(@Request() req: any): Promise<any> {
     if (process.env.REFERER) {
       if (!req.headers.referer?.startsWith(process.env.REFERER)) {
-        throw new ForbiddenException('Forbidden' };
+        throw new ForbiddenException('Forbidden');
       }
     }
     return await this.ampService.evaluateHtml(req.body.html);
